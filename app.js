ecf57fec4b43f81630ef6b43895f432f2e01bf50
mahboobe-Mhz <mahbo0be.mohammadzade@gmail.com>
2023-01-25 10:02:31 +0330

    answer Q5

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 "use strict";
+//^^^^^^^^^^^^^^^^^^^^^^^^^سوال -5 ^^^^^^^^^^^^^^^^^^
+
+ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+};
+ask(
+  "Do you agree ?",
+  () => {
+    alert("you agreed");
+  },
+  () => {
+    alert("you cancle the execution.");
+  }
+);
