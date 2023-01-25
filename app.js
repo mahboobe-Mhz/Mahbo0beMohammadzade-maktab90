4d760ca493905c051c6c51f99407ff5cf3648ec7
mahboobe-Mhz <mahbo0be.mohammadzade@gmail.com>
2023-01-25 09:57:26 +0330

    answer Q4

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 "use strict";
+//^^^^^^^^^^^^^^^^^^^^^^ سوال -4 ^^^^^^^^^^^^^^^^^^^^
+
+let arr = [];
+
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const fd = new FormData(form);
+  const obj = Object.fromEntries(fd);
+  console.log(obj);
+  showNotification(obj);
+});
+
+const span = document.getElementById("span");
+function showNotification(obj) {
+  span.textContent = `${obj.html}`;
+
+  const styleH = {
+    opacity: "100%",
+    position: "absolute",
+    right: `${obj.right}px`,
+    top: `${obj.top}px`,
+  };
+  Object.assign(span.style, styleH);
+}
